cafd4a04d224da71cd60a028b3b0ff31db459efe
grzegorz.kot <grzegorz.kot@dieboldnixdorf.com>
2019-09-12 15:34:15 +0200

    #2 - implementation of swagger

--- EmployeeModule/TherapyBackOffice_EmployeeModule/Startup.cs
+++ EmployeeModule/TherapyBackOffice_EmployeeModule/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace TherapyBackOffice_EmployeeModule.API
 {
@@ -18,6 +19,15 @@ namespace TherapyBackOffice_EmployeeModule.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Therapy BackOffice Management API"
+                });
+
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,6 +39,12 @@ namespace TherapyBackOffice_EmployeeModule.API
             }
 
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
         }
     }
 }
